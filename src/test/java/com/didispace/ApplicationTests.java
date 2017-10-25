77e8bca2c4682f361d2f9d29a65dc810b45e0dbc
myIrelia <myIrelia@foxmail.com>
2017-10-25 14:14:15 +0800

    Initial commit

---
+++ src/test/java/com/didispace/ApplicationTests.java
@@ -0,0 +1,39 @@
+package com.didispace;
+
+import com.didispace.domain.User;
+import com.didispace.domain.UserRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+public class ApplicationTests {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	public void test() throws Exception {
+
+		// 创建10条记录
+		userRepository.save(new User("AAA", 10));
+		userRepository.save(new User("BBB", 20));
+		userRepository.save(new User("CCC", 30));
+		userRepository.save(new User("DDD", 40));
+		userRepository.save(new User("EEE", 50));
+		userRepository.save(new User("FFF", 60));
+		userRepository.save(new User("GGG", 70));
+		userRepository.save(new User("HHH", 80));
+		userRepository.save(new User("III", 90));
+		userRepository.save(new User("JJJ", 100));
+
+
+	}
+
+
+}
